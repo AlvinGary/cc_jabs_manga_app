c58c26f45838de288501e6f974aba68594adf971
AlvinGary <alvingary201@gmail.com>
2022-12-13 22:05:48 +0700

    add splash page and connect to login page

---
+++ lib/views/pages/pages.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+import 'package:cc_jabs_manga_app/services/services.dart';
+import 'package:cc_jabs_manga_app/views/Widgets/widgets.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../models/models.dart';
+
+part 'splash.dart';
+part 'login.dart';
+part 'home.dart';
