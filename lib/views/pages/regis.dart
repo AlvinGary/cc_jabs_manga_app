2249406f066b056ab843bbaec089f979a7608560
AlvinGary <alvingary201@gmail.com>
2022-12-14 09:36:42 +0700

    add login regis ui

--- lib/views/pages/regis.dart
+++ lib/views/pages/regis.dart
@@ -1,17 +1,20 @@
 part of 'pages.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class RegisPage extends StatefulWidget {
+  const RegisPage({Key? key}) : super(key: key);
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _RegisPageState createState() => _RegisPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  final _loginkey = GlobalKey<FormState>();
+class _RegisPageState extends State<RegisPage> {
+  final _regiskey = GlobalKey<FormState>();
+  final ctrlUser = TextEditingController();
   final ctrlEmail = TextEditingController();
   final ctrlPass = TextEditingController();
+  final ctrlConfirmPass = TextEditingController();
   bool isHide = true;
+  bool isHide2 = true;
   bool isLoading = false;
 
   @override
@@ -21,18 +24,21 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   void dispose() {
+    ctrlUser.dispose();
     ctrlEmail.dispose();
     ctrlPass.dispose();
+    ctrlConfirmPass.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        // Here we take the value from the LoginPage object that was created by
+        // Here we take the value from the RegisPage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text("Login"),
+        title: Text("Register"),
         centerTitle: true,
       ),
       body: Stack(
@@ -40,14 +46,31 @@ class _LoginPageState extends State<LoginPage> {
           Container(
             // color: Colors.orange,
             width: double.infinity,
-            height: 400,
+            height: 420,
             padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
             child: Center(
               child: Form(
-                key: _loginkey,
+                key: _regiskey,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
+                    TextFormField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      controller: ctrlUser,
+                      decoration: InputDecoration(
+                        labelText: "Username",
+                        prefixIcon: Icon(Icons.person_outline),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                      ),
+                      validator: (value) {
+                        return value!.length < 4
+                            ? "Username must at least 4 characters!"
+                            : null;
+                      },
+                    ),
+                    SizedBox(height: 16),
                     TextFormField(
                       keyboardType: TextInputType.emailAddress,
                       autovalidateMode: AutovalidateMode.onUserInteraction,
@@ -65,7 +88,7 @@ class _LoginPageState extends State<LoginPage> {
                             : null;
                       },
                     ),
-                    SizedBox(height: 32),
+                    SizedBox(height: 16),
                     TextFormField(
                       obscureText: isHide,
                       autovalidateMode: AutovalidateMode.onUserInteraction,
@@ -93,7 +116,35 @@ class _LoginPageState extends State<LoginPage> {
                             : null;
                       },
                     ),
-                    SizedBox(height: 42),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      obscureText: isHide2,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      controller: ctrlConfirmPass,
+                      decoration: InputDecoration(
+                        labelText: "Confirm Password",
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                        ),
+                        suffixIcon: new GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isHide2 = !isHide2;
+                            });
+                          },
+                          child: Icon(
+                            isHide2 ? Icons.visibility : Icons.visibility_off,
+                          ),
+                        ),
+                      ),
+                      validator: (value) {
+                        return value!.length < 6
+                            ? "Password must at least 6 characters!"
+                            : null;
+                      },
+                    ),
+                    SizedBox(height: 16),
                     Container(
                       width: double.infinity,
                       child: ElevatedButton(
@@ -102,7 +153,7 @@ class _LoginPageState extends State<LoginPage> {
                             Navigator.pushReplacement(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => HomePage()));
+                                    builder: (context) => LoginPage()));
                           },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Colors.orange,
@@ -111,28 +162,12 @@ class _LoginPageState extends State<LoginPage> {
                               fontSize: 20,
                               fontWeight: FontWeight.bold,
                             ),
-                            padding: EdgeInsets.fromLTRB(24, 14, 24, 12),
+                            padding: EdgeInsets.fromLTRB(24, 12, 24, 12),
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10),
                             ),
                           ),
-                          child: Text("LOGIN")),
-                    ),
-                    SizedBox(height: 16),
-                    Align(
-                      alignment: Alignment(0, 0.89),
-                      child: GestureDetector(
-                        onTap: () async {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => RegisPage()));
-                        },
-                        child: Text(
-                          "Don't have account? Sign up here!",
-                          style: TextStyle(color: Colors.blue, fontSize: 14),
-                        ),
-                      ),
+                          child: Text("REGISTER")),
                     ),
                   ],
                 ),
--- lib/views/widgets/widgets.dart
+++ lib/views/widgets/widgets.dart
@@ -2,3 +2,7 @@ import 'package:cc_jabs_manga_app/models/models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+
+part 'uitoast.dart';
+part 'uiloading.dart';
+part 'menubar.dart';
