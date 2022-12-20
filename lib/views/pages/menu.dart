68069afc336e7e50c5f2454b4b9073a3677c5791
AlvinGary <alvingary201@gmail.com>
2022-12-20 22:15:39 +0700

    add app icon, fix profile page and navbar menu

---
+++ lib/views/pages/menu.dart
@@ -0,0 +1,66 @@
+part of 'pages.dart';
+
+class MenuPage extends StatefulWidget {
+  const MenuPage({Key? key}) : super(key: key);
+
+  @override
+  _MenuPageState createState() => _MenuPageState();
+}
+
+class _MenuPageState extends State<MenuPage> {
+  int _selectedIndex = 0;
+  static List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    SearchPage(),
+    LibraryPage(),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      /*
+      ** Bottom Navigation Bar Menu
+      */
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.orange,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Library',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- lib/views/pages/profile.dart
+++ lib/views/pages/profile.dart
@@ -11,47 +11,153 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  Stack(
-          children: [
-            Container(
-              width: double.infinity,
-              height: double. infinity,
-              padding: EdgeInsets.all(24),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    //Icon masih sebagai placeholder untuk user image nanti
-
-                    Icon(
-                      Icons.person_rounded,
-                      color: Colors.black,
-                      size: 100.0
-
-                      ),
-                    SizedBox(height: 16,),
-                    TextField(
-                      readOnly: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Username",
-                      ),
-                    ),
-                    SizedBox(height: 16,),
-                    TextField(
-                      readOnly: true,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Password',
+      appBar: AppBar(
+        title: Text('Profile'),
+        actions: [
+          (AuthService.auth.currentUser != null)
+              ? IconButton(
+                  onPressed: () async {
+                    await AuthService.SignOut().then((value) {
+                      if (value) {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage()));
+                        UiToast.toastOk("Logout successful!");
+                      } else {
+                        UiToast.toastErr("Logout failed!");
+                      }
+                    });
+                  },
+                  icon: Icon(Icons.logout))
+              : Container()
+        ],
+      ),
+      body: Stack(
+        children: [
+          (AuthService.auth.currentUser != null)
+              ? Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  padding: EdgeInsets.all(24),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 65,
+                          backgroundColor: Colors.orange,
+                          child: CircleAvatar(
+                            radius: 60,
+                            backgroundImage: NetworkImage(
+                                "${AuthService.auth.currentUser!.photoURL}"),
+                          ),
                         ),
+                        SizedBox(
+                          height: 24,
+                        ),
+                        TextField(
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelStyle:
+                                TextStyle(color: Colors.black, fontSize: 20),
+                            labelText:
+                                "${AuthService.auth.currentUser!.displayName}",
+                          ),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        TextField(
+                          readOnly: true,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelStyle:
+                                TextStyle(color: Colors.black, fontSize: 20),
+                            labelText: "${AuthService.auth.currentUser!.email}",
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
+                  ),
+                )
+              : Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  padding: EdgeInsets.all(24),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        //Icon masih sebagai placeholder untuk user image nanti
+                        Icon(Icons.person_rounded,
+                            color: Colors.black, size: 100.0),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        TextField(
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Username",
+                          ),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        TextField(
+                          readOnly: true,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Email",
+                          ),
+                        ),
+                        SizedBox(height: 24),
+                        Container(
+                          width: double.infinity,
+                          child: ElevatedButton.icon(
+                            onPressed: () async {
+                              await AuthService.signInWithGoogle()
+                                  .then((value) {
+                                UiToast.toastOk("Welcome back");
+                              });
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => MenuPage()));
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.orange,
+                                elevation: 3,
+                                textStyle: TextStyle(fontSize: 16),
+                                shadowColor: Colors.black,
+                                padding: EdgeInsets.fromLTRB(24, 10, 24, 10),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                            label: Text(
+                              'Login Now',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            icon: FaIcon(
+                              FontAwesomeIcons.google,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+        ],
+      ),
     );
   }
 }
